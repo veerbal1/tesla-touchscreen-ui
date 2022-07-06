9d569af0dfae63cbf3088648a39edc49e2190eec
veerbal1 <veerbal1singh1@gmail.com>
2022-07-06 13:02:25 +0530

    Created Top info bar in sidebar

---
+++ src/components/sections/SideBar/TopInfoBar.js
@@ -0,0 +1,37 @@
+import { css } from '@emotion/css';
+import React from 'react';
+import IconButton from '../../buttons/Icon';
+import HeadLights from '../../icons/HeadLights';
+import TirePressure from '../../icons/TirePressure';
+
+const TopInfoBar = () => {
+  return (
+    <div
+      className={css`
+        display: flex;
+        justify-content: space-between;
+        padding: 4px 16px;
+      `}
+    >
+      <IconButton
+        onClick={() => {
+          console.log('Tire Pressure');
+        }}
+        className={css`
+          margin-left: 4em;
+        `}
+      >
+        <TirePressure />
+      </IconButton>
+      <IconButton
+        onClick={() => {
+          console.log('Tire Pressure');
+        }}
+      >
+        <HeadLights />
+      </IconButton>
+    </div>
+  );
+};
+
+export default TopInfoBar;
--- src/components/sections/SideBar/index.js
+++ src/components/sections/SideBar/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { css } from '@emotion/css';
+import TopInfoBar from './TopInfoBar';
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div
+      className={css`
+        width: 100%;
+        height: 100%;
+      `}
+    >
+      <TopInfoBar />
+    </div>
+  );
 };
 
 export default SideBar;
