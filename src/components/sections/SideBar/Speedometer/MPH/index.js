721484a37235c95c6e2097218959dd0af35b61e5
veerbal1 <veerbal1singh1@gmail.com>
2022-07-06 13:37:17 +0530

    Created MPH

---
+++ src/components/sections/SideBar/Speedometer/MPH/index.js
@@ -0,0 +1,16 @@
+import { css } from '@emotion/css';
+import React from 'react';
+
+const MPH = () => {
+  return (
+    <span
+      className={css`
+        color: rgba(0, 0, 0, 0.4);
+      `}
+    >
+      MPH
+    </span>
+  );
+};
+
+export default MPH;
