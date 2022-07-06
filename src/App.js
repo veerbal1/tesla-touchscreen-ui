ea04f1218b8a110891cf644802893c9a0be61824
veerbal1 <veerbal1singh1@gmail.com>
2022-07-06 08:51:00 +0530

    Initial commit with react-template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <h1
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Veerbal
+      </h1>
+    </div>
+  );
+}
+
+export default App;
